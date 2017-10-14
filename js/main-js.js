d16ee803f54b0f730c91b878f059aa7c965144ff
MishaChernov <80amulet63@gmail.com>
2017-10-15 02:26:23 +0300

    Update 2017-10-14T23:26:16.834Z

--- js/main-js.js
+++ js/main-js.js
@@ -1,8 +1,48 @@
 'use strict'
 
-$('.no-js').removeClass('no-js');
-
-$('.burger').click(function() {
-  $(this).toggleClass('burger--close  burger--open');
-  $('.main-menu__list').slideToggle();
+$('.slider').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: true,
+ fade: true,
+ asNavFor: '.slides__wrapper'
+});
+$('.slides__wrapper').slick({
+ slidesToShow: 4,
+ slidesToScroll: 1,
+ arrows: false,
+ asNavFor: '.slider',
+ centerMode: true,
+ focusOnSelect: true,
+ responsive: [{
+     breakpoint: 1190,
+     settings: {
+       slidesToShow: 5,
+       slidesToScroll: 1,
+     }
+   },
+   {
+     breakpoint: 850,
+     settings: {
+       slidesToShow: 4,
+       slidesToScroll: 1,
+     }
+   },
+   {
+     breakpoint: 650,
+     settings: {
+       slidesToShow: 3,
+       slidesToScroll: 1,
+     }
+   },
+   {
+     breakpoint: 400,
+     settings: {
+       arrows: true,
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       arrows: false,
+     }
+   }
+ ]
 });
